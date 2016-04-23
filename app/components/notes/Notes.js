ebc6107b0210f25045bc0948d202d241a069365f
pjakubowski <ismc2222@gmail.com>
2016-04-23 18:16:15 +0100

    adds validation to repos userprofile notes

--- app/components/notes/Notes.js
+++ app/components/notes/Notes.js
@@ -1,15 +1,20 @@
-var React = require('react');
+var React = require('react')
+var NotesList = require('./NotesList')
 
 var Notes = React.createClass({
-  render: function(){
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    notes: React.PropTypes.array.isRequired
+  },
+  render: function () {
+    console.log('Notes ----->', this.props.notes)
     return (
       <div>
-        <p>NOTES </p>
-        <p> {this.props.notes}</p>
-
+        <h3>Notes for {this.props.username}</h3>
+        <NotesList notes={this.props.notes} />
       </div>
     )
   }
 })
 
-module.exports = Notes;
+module.exports = Notes
