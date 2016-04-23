478425a828b12f994da6096f19ab2ceea70c4e04
pjakubowski <ismc2222@gmail.com>
2016-04-23 18:54:59 +0100

    adds handle note

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var Router = require('react-router');
+var React = require('react')
+var Router = require('react-router')
 var Repos = require('./github/Repos')
 var UserProfile = require('./github/UserProfile')
 var Notes = require('./notes/Notes')
@@ -10,7 +10,7 @@ var Profile = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function () {
     return {
-      notes: [1,2,3],
+      notes: [1, 2, 3],
       bio: {
         name: 'Tyler McGinnis'
       },
@@ -25,7 +25,10 @@ var Profile = React.createClass({
   componentWillUnmount: function () {
     this.unbind('notes')
   },
-  render: function(){
+  handleAddNote: function (newNote) {
+    this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
+  },
+  render: function () {
     console.log('Prifile props -----> ', this.props)
     return (
       <div className="row">
@@ -36,7 +39,10 @@ var Profile = React.createClass({
           <Repos username={this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
-          <Notes username={this.props.params.username} notes={this.state.notes} />
+          <Notes
+            username={this.props.params.username}
+            notes={this.state.notes}
+            addNote={this.handleAddNote} />
         </div>
       </div>
     )
