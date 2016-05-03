03c41dcfa1828c5299effc4c0358eaf5394b3bdf
pjakubowski <ismc2222@gmail.com>
2016-05-04 00:08:51 +0100

    adds delete, save undo buttons and event handlers

--- app/components/iptv/Table.js
+++ app/components/iptv/Table.js
@@ -3,40 +3,31 @@ var TableRow = require('./TableRow')
 
 var Table = React.createClass({
 
-  getInitialState: function() {
-    return {
-      rooms: this.props.rooms
-    }
-  },
 
   render: function () {
-
+    console.log('table props', this.props.rooms)
     return (
       <div>
-        <div className='table-responsive'>
-      <table className='table table-hover'>
+        <table>
         <thead>
           <tr>
-            <th>Room Id</th>
             <th>Room Name</th>
+            <th>Guests</th>
             <th>Check In</th>
             <th>Check Out</th>
-            <th>Guest Name</th>
-            <th>Site Name</th>
           </tr>
-
         </thead>
-      <tbody>
-        {this.props.rooms.map(function (room, i) {
-          return <TableRow key={i} data={room} />
-        })
-      }
-</tbody>
-      </table>
-    </div>
-    </div>
+        <tbody>
+              {this.props.rooms.map(function (room, i) {
+                return <TableRow key={i} data={room} />
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     )
-  }
+  },
+
 })
 
 module.exports = Table
--- app/components/iptv/TableRow.js
+++ app/components/iptv/TableRow.js
@@ -1,25 +1,118 @@
 var React = require('react')
 import Time from 'react-time'
+import helpers from '../../utils/helpers'
 
 var TableRow = React.createClass({
+  getInitialState: function () {
+    return {
+      check_in_date: this.props.data.check_in_date,
+      check_out_date: this.props.data.check_out_date,
+      room_guests: this.props.data.room_guests,
+      site_name: this.props.data.site_name,
+      room_name: this.props.data.room_name,
+      textChanged: false
+    }
+  },
+  handleTextChange: function(event) {
+   this.setState({
+     room_guests: event.target.value,
 
-  render: function () {
+     textChanged: true
+   });
+ },
+ handleSaveClick: function() {
+    this.setState({textChanged: false});
+  },
+  handleUndoClick: function() {
+    this.setState({
+      room_guests: this.props.data.room_guests,
+      check_in_date: this.props.data.check_in_date,
+      check_out_date: this.props.data.check_out_date,
+      textChanged: false
+    });
+  },
+  handleDeleteClick: function() {
+    this.setState({
+      room_guests: '',
+      check_in_date: '',
+      check_out_date: ''
+    })
+  },
+ changesButtons: function() {
+    if(!this.state.textChanged) {
+      return null
+    } else {
+      return [
+        <button
+          onClick={this.handleSaveClick}
+          className="btn btn-default"
+          >
+          Save
+        </button>,
+        <button
+          onClick={this.handleUndoClick}
+          className="btn btn-default"
+          >
+          Undo
+        </button>
+      ]
+    }
+  },
+  render: function() {
     return (
       <tr>
+        <td><input type="text"
+          disabled={true}
+          className="form-control"
+          value={this.state.room_name}
+          /></td>
+          <td><input type="text"
+            className="form-control"
+            value={this.state.room_guests}
+            onChange={this.handleTextChange}
+            /></td>
+        <td><input type="text"
+          className="form-control"
+          value={this.state.check_in_date}
 
-        <td>{this.props.data.id}</td>
-        <td><a href={`file:///Users/pawel/Desktop/projects/newaya/areception2/public/index.html?#/room/${this.props.data.room_name}`} > {this.props.data.room_name} </a></td>
-        <td><Time value={this.props.data.check_in_date} format='DD-MM-YYYY'/></td>
-        <td><Time value={this.props.data.check_out_date} format='DD-MM-YYYY'/></td>
-        <td>
-          <div className='label label-primary'>{this.props.data.room_guests[0]}</div>
-          <br />
-          <div className='label label-primary'>{this.props.data.room_guests[1]}</div>
-        </td>
-        <td>{this.props.data.site_name}</td>
+          /></td>
+        <td><input type="text"
+          className="form-control"
+          value={this.state.check_out_date}
+
+          /></td>
+        <td><span className="input-group-btn">
+          {this.changesButtons()}
+          <button
+            onClick={this.handleDeleteClick}
+            className="btn btn-primary"
+            >
+            Delete
+          </button>
+        </span></td>
       </tr>
     )
   }
+
+  // render: function () {
+  //   return (
+  //
+  //     <tr>
+  //       <td>{this.props.data.id}</td>
+  //       <td><a href={`file:///Users/pawel/Desktop/projects/newaya/areception2/public/index.html?#/room/${this.props.data.room_name}`} > {this.props.data.room_name} </a></td>
+  //
+  //     <td><Time value={this.props.data.check_in_date} format='DD-MM-YYYY'/></td>
+  //       <td><Time value={this.props.data.check_out_date} format='DD-MM-YYYY'/></td>
+  //       <td>
+  //         <div className='label label-primary'>{this.props.data.room_guests[0]}</div>
+  //         <br />
+  //         <div className='label label-primary'>{this.props.data.room_guests[1]}</div>
+  //       </td>
+  //       <td>{this.props.data.site_name}</td>
+  //     </tr>
+  //
+  //   )
+  // }
 })
 
 module.exports = TableRow
