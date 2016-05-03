ac04e89f0f2123fd372827938b1b3e4ce5868887
pjakubowski <ismc2222@gmail.com>
2016-05-04 00:12:00 +0100

    adds messages data

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -4,16 +4,20 @@ function getRooms () {
   return axios.get(`http://localhost:3009/api/reception/v1/rooms.json`)
 }
 
-function getReservations (username) {
+function getReservations () {
   return axios.get(`http://localhost:3009/api/reception/v1/reservations`)
+}
+function getGuestMessages () {
+  return axios.get(`http://localhost:3009/api/reception/v1/guest_messages.json`)
 }
 
 var helpers = {
   getIptvInfo: function () {
-    return axios.all([getRooms(), getReservations()]).then(function(arr){
+    return axios.all([getRooms(), getReservations(), getGuestMessages()]).then(function(arr){
       return {
         rooms: arr[0].data,
-        reservations: arr[1].data
+        reservations: arr[1].data,
+        messages: arr[2].data
       }
   })
 }
